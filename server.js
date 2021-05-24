d74bc098b515f03bcb58813cc351e9f2cc5c5ab4
Toby Saunders <toby.saunders@live.co.uk>
2021-05-24 15:53:43 +0100

    changing response

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => { res.send('it is working!')});
+
 app.post('/signin', (signin.handleSignin(db, bcrypt)));
 
 app.post('/register', (register.handleRegister(db, bcrypt)));
