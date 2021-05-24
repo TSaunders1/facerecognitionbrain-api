deb6cf09d2b2ab84e6074f4f7de7abee014b5608
Toby Saunders <toby.saunders@live.co.uk>
2021-05-24 16:13:54 +0100

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-horizontal-76795',
     user : 'tobysaunders',
     password : '',
     database : 'smart-brain'
